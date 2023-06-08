af251d113a9775270d19424fac0deb752d65b457
Saiful082 <smsaifulislam082@.com>
2023-06-08 16:34:36 +0600

    news show display and context setup

--- src/Pages/Category/Category/Category.js
+++ src/Pages/Category/Category/Category.js
@@ -1,15 +1,22 @@
 import React, {  } from 'react';
 import { useLoaderData } from 'react-router-dom';
+import NewsSummaryCard from '../../Shared/NewsSummaryCard/NewsSummaryCard';
 
 const Category = () => {
 
 
-    const news = useLoaderData();
+    const categoryNews = useLoaderData();
 
 
     return (
         <div>
-            <h2>This is Category hsa news: {news.length}</h2>
+            <h2>This is Category hsa news: {categoryNews.length}</h2>
+            {
+               categoryNews.map(news => <NewsSummaryCard
+                    key={news._id}
+                    news={news}
+               ></NewsSummaryCard>)
+            }
         </div>
     );
 };
